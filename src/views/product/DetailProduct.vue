2275d0d380645892bcabcf14f62841f1553cc561
rans-hekel <ransenju27@gmail.com>
2024-01-06 15:50:23 +0700

    commit kedua untuk menambahkan products index dari seed

--- src/views/product/DetailProduct.vue
+++ src/views/product/DetailProduct.vue
@@ -10,6 +10,40 @@
   }
   </script>
   
-  <style>
-  
+  <style scoped>
+  #page-wrap {
+    margin-top: 16px;
+    padding: 16px;
+    max-width: 600px;
+  }
+
+  #img-wrap {
+    text-align: center;;
+  }
+
+  img {
+    width: 400px;
+  }
+
+  #product-details {
+    padding: 16px;
+    position: relative;
+  }
+
+  #add-to-cart {
+    width: 100%;
+  }
+
+  #price {
+    position: absolute;
+    top: 24px;
+    right: 16px;
+  }
+  .notif {
+    text-align: center;
+    color: white;
+    background-color:#41B883;
+    padding: 3%;
+    border-radius: 8px;
+  }
   </style>
